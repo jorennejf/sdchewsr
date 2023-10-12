c5f06276d5906de182549666e6e7b47015172a89
Jorenne Flores <jorennejf@gmail.com>
2023-10-12 16:41:15 -0700

    restaurants and other updates

--- js/chewsr.js
+++ js/chewsr.js
@@ -1,5 +1,11 @@
 var list = [];
-list.push({'name':'Sushi Diner', 'location': 'Mesa College', 'cuisine': 'Japanese'});
+list.push({'name':'La Fuente', 'location': 'Serra Mesa', 'cuisine': 'Latin'});
+list.push({'name':'Pick Up Stix', 'location': 'Serra Mesa', 'cuisine': 'Chinese'});
+list.push({'name':'Rubio\'s', 'location': 'Serra Mesa', 'cuisine': 'Latin'});
+list.push({'name':'Tahini', 'location': 'Serra Mesa', 'cuisine': 'Mediterranean'});
+list.push({'name':'Lit\'l Pepper', 'location': 'Serra Mesa', 'cuisine': 'American'});
+list.push({'name':'Village Indian Cuisine', 'location': 'Serra Mesa', 'cuisine': 'Indian'});
+list.push({'name':'Sonic', 'location': 'Serra Mesa', 'cuisine': 'American'});
 list.push({'name':'Homestyle Hawaiian', 'location': 'Mesa College', 'cuisine': 'Hawaiian'});
 list.push({'name':'Domino\'s Pizza', 'location': 'Mesa College', 'cuisine': 'American'});
 list.push({'name':'K\'s Sandwiches', 'location': 'Mesa College', 'cuisine': 'Vietnamese'});
@@ -8,14 +14,18 @@ list.push({'name':'Dim Sum', 'location': 'Convoy', 'cuisine': 'Chinese'});
 list.push({'name':'H-Mart Food Court', 'location': 'Convoy', 'cuisine': 'Korean'});
 list.push({'name':'Cross Street', 'location': 'Convoy', 'cuisine': 'Korean'});
 list.push({'name':'Popeyes', 'location': 'Convoy', 'cuisine': 'American'});
+list.push({'name':'Popeyes', 'location': 'Serra Mesa', 'cuisine': 'American'}); //duplicate
 list.push({'name':'Phuong Trang', 'location': 'Convoy', 'cuisine': 'Vietnamese'});
 list.push({'name':'Koon Thai', 'location': 'Convoy', 'cuisine': 'Thai'});
 list.push({'name':'Coco Ichibanya', 'location': 'Convoy', 'cuisine': 'Japanese'});
 list.push({'name':'Nishiki Ramen', 'location': 'Convoy', 'cuisine': 'Japanese'});
 list.push({'name':'Poki 1/2', 'location': 'Convoy', 'cuisine': 'Hawaiian'});
 list.push({'name':'Costco', 'location': 'Convoy', 'cuisine': 'American'});
+list.push({'name':'Costco', 'location': 'Mission Valley', 'cuisine': 'American'});
 list.push({'name':'Taco Stand', 'location': 'Convoy', 'cuisine': 'Latin'});
 list.push({'name':'In N Out', 'location': 'Convoy', 'cuisine': 'American'});
+list.push({'name':'In N Out', 'location': 'Mission Valley', 'cuisine': 'American'}); //duplicate
+list.push({'name':'The Great Greek', 'location': 'Convoy', 'cuisine': 'Mediterranean'});
 list.push({'name':'Katsu Cafe', 'location': 'Clairemont Mesa', 'cuisine': 'Japanese'});
 list.push({'name':'Ajisen Ramen', 'location': 'Clairemont Mesa', 'cuisine': 'Japanese'});
 list.push({'name':'Arby\'s', 'location': 'Clairemont Mesa', 'cuisine': 'American'});
@@ -32,16 +42,19 @@ list.push({'name':'Chaba Thai', 'location': 'Clairemont Mesa', 'cuisine': 'Thai'
 list.push({'name':'Chili\'s', 'location': 'Clairemont Mesa', 'cuisine': 'American'});
 list.push({'name':'Domino\'s', 'location': 'Clairemont Mesa', 'cuisine': 'American'}); //duplicate
 list.push({'name':'Dino\'s Gyros', 'location': 'Clairemont Mesa', 'cuisine': 'Mediterranean'});
-list.push({'name':'Tahini', 'location': 'Clairemont Mesa', 'cuisine': 'Mediterranean'});
-list.push({'name':'Lit\'l Pepper', 'location': 'Clairemont Mesa', 'cuisine': 'American'});
-list.push({'name':'Village Indian Cuisine', 'location': 'Clairemont Mesa', 'cuisine': 'Indian'});
-list.push({'name':'Sonic', 'location': 'Clairemont Mesa', 'cuisine': 'American'});
-list.push({'name':'McDonald\'s', 'location': 'Clairemont Mesa', 'cuisine': 'American'});
+list.push({'name':'McDonald\'s', 'location': 'Serra Mesa', 'cuisine': 'American'});
 list.push({'name':'McDonald\'s', 'location': 'Balboa', 'cuisine': 'American'}); //duplicate
+list.push({'name':'McDonald\'s', 'location': 'Serra Mesa', 'cuisine': 'American'}); //duplicate
 list.push({'name':'Taco Bell', 'location': 'Clairemont Mesa', 'cuisine': 'Latin'});
 list.push({'name':'Taco Bell', 'location': 'Balboa', 'cuisine': 'Latin'}); //duplicate
-list.push({'name':'Boston Market', 'location': 'Balboa', 'cuisine': 'American'});
+list.push({'name':'Taco Bell', 'location': 'Serra Mesa', 'cuisine': 'Latin'}); //duplicate
+//list.push({'name':'Boston Market', 'location': 'Balboa', 'cuisine': 'American'}); //permanently closed
 list.push({'name':'Panera Bread', 'location': 'Balboa', 'cuisine': 'American'});
+list.push({'name':'Panera Bread', 'location': 'Serra Mesa', 'cuisine': 'American'}); //duplicate
+list.push({'name':'Panera Bread', 'location': 'Mission Valley', 'cuisine': 'American'}); //duplicate
+list.push({'name':'Ikea', 'location': 'Mission Valley', 'cuisine': 'American'});
+list.push({'name':'Cava', 'location': 'Mission Valley', 'cuisine': 'Mediterranean'});
+list.push({'name':'The Cheesecake Factory', 'location': 'Mission Valley', 'cuisine': 'American'});
 list.push({'name':'The Habit', 'location': 'Balboa', 'cuisine': 'American'});
 list.push({'name':'Five Guys', 'location': 'Balboa', 'cuisine': 'American'});
 list.push({'name':'Dickey\'s BBQ', 'location': 'Balboa', 'cuisine': 'American'});
@@ -50,6 +63,9 @@ list.push({'name':'Thai Time', 'location': 'Balboa', 'cuisine': 'Thai'});
 list.push({'name':'Luna Grill', 'location': 'Balboa', 'cuisine': 'Mediterranean'});
 list.push({'name':'Vallarta\'s', 'location': 'Balboa', 'cuisine': 'Latin'});
 list.push({'name':'China Quick Wok', 'location': 'Balboa', 'cuisine': 'Chinese'});
+list.push({'name':'Mike\'s Red Tacos', 'location': 'Balboa', 'cuisine': 'Latin'});
+list.push({'name':'Chick-Fil-A', 'location': 'Balboa', 'cuisine': 'American'});
+list.push({'name':'Chick-Fil-A', 'location': 'Mission Valley', 'cuisine': 'American'}); //duplicate
 list.push({'name':'Hawaiian Fresh Seafood', 'location': 'Mira Mesa', 'cuisine': 'Hawaiian'});
 list.push({'name':'Jollibee', 'location': 'Mira Mesa', 'cuisine': 'Filipino'});
 list.push({'name':'Grill City', 'location': 'Mira Mesa', 'cuisine': 'Filipino'});
@@ -57,10 +73,11 @@ list.push({'name':'R&B Cuisine', 'location': 'Mira Mesa', 'cuisine': 'Filipino'}
 list.push({'name':'Raising Cane\'s', 'location': 'Mira Mesa', 'cuisine': 'American'});
 list.push({'name':'LouZiana', 'location': 'Mira Mesa', 'cuisine': 'American'});
 list.push({'name':'Charminar', 'location': 'Mira Mesa', 'cuisine': 'Indian'});
-list.push({'name':'La Fuente', 'location': 'Other', 'cuisine': 'Latin'});
+list.push({'name':'Sushi Diner 2', 'location': 'Other', 'cuisine': 'Japanese'});
 list.push({'name':'Ike\'s Sandwiches', 'location': 'Other', 'cuisine': 'American'});
 list.push({'name':'Wingstop', 'location': 'Other', 'cuisine': 'American'});
-list.push({'name':'In N Out', 'location': 'Other', 'cuisine': 'American'}); //duplicate
+list.push({'name':'Cava', 'location': 'Other', 'cuisine': 'Mediterranean'});
+list.push({'name':'Public Square Coffee', 'location': 'Other', 'cuisine': 'American'});
 //TODO what to do with duplicates?
 
 //TODO clean up variable names?
@@ -200,17 +217,23 @@ function closeFilters(){
 
 $(document).ready(function() {
     //display message on page load
-    $('#startupModal').modal('show');
+    //$('#startupModal').modal('show');
 
     //creates human readable list from js array
+    var serra = '';
     var college = '';
     var convoy = '';
+    var mission = '';
     var clairemont = '';
     var balboa = '';
     var mira = '';
     var other = '';
     for(var i=0; i<list.length; i++){
         switch(list[i].location){
+            case 'Serra Mesa':
+                if(serra.length == 0) serra = list[i].name;
+                else serra = serra + ', ' + list[i].name;
+                break;
             case 'Mesa College':
                 if(college.length == 0) college = list[i].name;
                 else college = college + ', ' + list[i].name;
@@ -218,6 +241,10 @@ $(document).ready(function() {
             case 'Convoy':
                 if(convoy.length == 0) convoy = list[i].name;
                 else convoy = convoy + ', ' + list[i].name;
+                break;
+            case 'Mission Valley':
+                if(mission.length == 0) mission = list[i].name;
+                else mission = mission + ', ' + list[i].name;
                 break;
             case 'Clairemont Mesa':
                 if(clairemont.length == 0) clairemont = list[i].name;
@@ -237,10 +264,14 @@ $(document).ready(function() {
                 break;
         }
     }
-    var data = '<br/><h6>Mesa College</h6>'
+    var data = '<br/><h6>Serra Mesa</h6>'
+                    + serra
+                + '<br/><br/><h6>Mesa College</h6>'
                     + college
                 + '<br/><br/><h6>Convoy</h6>'
                     + convoy
+                + '<br/><br/><h6>Mission Valley</h6>'
+                    + mission
                 + '<br/><br/><h6>Clairemont Mesa</h6>'
                     + clairemont
                 + '<br/><br/><h6>Balboa</h6>'
